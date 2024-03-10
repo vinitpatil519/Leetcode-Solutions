dcfbbd06f22d63a00ee1d0411a412fdf54501b86
vinitpatil519 <141259539+vinitpatil519@users.noreply.github.com>
2024-03-10 22:16:07 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,19 +1,22 @@
+// two pointer approach/ dutch national flag approach
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int low = 0, mid = 0, high = nums.size()-1;
-        while(mid <= high){
+        int left = 0, index = 0, right = nums.size()-1;
+        //variable index is for traversing through the array
+        //variables left and right are the two pointers starting from 1st and the last element resp.
+        while(index <= right){
             if(nums[mid] == 0){
-                swap(nums[low], nums[mid]);
-                low++;
-                mid++;
+                swap(nums[low], nums[index]);
+                left++;
+                index++;
             }
-            else if(nums[mid] == 1){
-                mid++;
+            else if(nums[index] == 1){
+                index++;
             }
             else{
-                swap(nums[mid], nums[high]);
-                high--;
+                swap(nums[index], nums[right]);
+                right--;
             }
         }
     }
