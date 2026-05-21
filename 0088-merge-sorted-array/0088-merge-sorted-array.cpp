9588c1dbcc7207a8b617ba3122d3c37e067332ec
vinitpatil519 <141259539+vinitpatil519@users.noreply.github.com>
2026-05-22 02:26:56 +0530

    Time: 0 ms (100%), Space: 12.3 MB (40.22%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;  //to iterate thru resulting array (updated nums1)
+        
+        //sorting from decreasing order but from last to first --> indirectly its ascending only
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
+            } else {
+                nums1[k--] = nums2[j--];
+            }
+        }
+    }
+};
