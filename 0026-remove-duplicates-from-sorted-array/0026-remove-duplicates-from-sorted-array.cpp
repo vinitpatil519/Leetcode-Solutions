1fc9b1e36ea7091f7782e0e5ab8e5d234819d403
vinitpatil519 <141259539+vinitpatil519@users.noreply.github.com>
2026-05-21 23:09:48 +0530

    Time: 0 ms (100%), Space: 22.5 MB (97.52%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i=1, j=0;
-        // two pointers
-        while(i<nums.size()){
-            if(nums[i] == nums[j]) i++;
-            else {
-                j++;
-                nums[j] = nums[i];
+        int n = nums.size(), relocate_head = 0, unique = 1, i = 1;
+        while(i < n ){
+            if(nums[i] == nums[i-1]){    // adj element is duplicate
                 i++;
+                continue;
             }
+            // ----- unique element is found here -----
+            nums[relocate_head + 1] = nums[i];
+            i++;
+            relocate_head++;
+            unique++;
         }
-        return j+1;
+        return unique;
     }
 };
